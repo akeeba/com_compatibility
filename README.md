420ed8493a619825e984813278dd7a5b924d79ff
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-05-25 16:00:33 +0300

    Skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# Version Compatibility Page Generator
+ 
+This is an internal project for our site. It goes through the Akeeba Release System records and figures out which
+release of each component is compatible with which version of PHP and Joomla! / WordPress, generating the Compatibility
+page automatically.
---
+++ component/frontend/compatibility.php
@@ -0,0 +1,10 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+if (!defined('FOF30_INCLUDED') && !@include_once(JPATH_LIBRARIES . '/fof30/include.php'))
+{
+	throw new RuntimeException('FOF 3.0 is not installed', 500);
+}
+
+FOF30\Container\Container::getInstance('com_compatibility')->dispatcher->dispatch();
