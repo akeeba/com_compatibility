9f12c0491298e6ca6b80b0523b9555ed7e33abc6
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-05-25 16:16:59 +0300

    Add some basic backend stuff

---
+++ component/backend/Controller/ControlPanel.php
@@ -0,0 +1,24 @@
+<?php
+namespace Akeeba\Compatibility\Admin\Controller;
+
+
+use FOF30\Container\Container;
+use FOF30\Controller\Controller;
+use FOF30\Controller\Mixin\PredefinedTaskList;
+
+class ControlPanel extends Controller
+{
+	use PredefinedTaskList;
+
+	public function __construct(Container $container, array $config = array())
+	{
+		parent::__construct($container, $config);
+
+		$this->setPredefinedTaskList(['controlpanel']);
+	}
+
+	public function controlpanel($cachable = false)
+	{
+
+	}
+}
---
+++ component/backend/Model/ControlPanel.php
@@ -0,0 +1,8 @@
+<?php
+namespace Akeeba\Compatibility\Admin\Model;
+
+use FOF30\Model\Model;
+
+class ControlPanel extends Model
+{
+}
