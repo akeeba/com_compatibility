83b9bae76e703ffec9e606f223c7efbfd4be6906
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-12-19 12:50:46 +0200

    Added support for Dark Mode

--- component/backend/Dispatcher/Dispatcher.php
+++ component/backend/Dispatcher/Dispatcher.php
@@ -22,4 +22,28 @@ class Dispatcher extends FOFDispatcher
 		parent::__construct($container, $config);
 	}
 
+	public function onBeforeDispatch(): bool
+	{
+		// Renderer options (0=none, 1=frontend, 2=backend, 3=both)
+		$useFEF   = in_array($this->container->params->get('load_fef', 3), [2, 3]);
+		$fefReset = $useFEF && in_array($this->container->params->get('fef_reset', 3), [2, 3]);
+
+		if (!$useFEF)
+		{
+			$this->container->rendererClass = '\\FOF30\\Render\\Joomla3';
+		}
+
+		$darkMode = $this->container->params->get('dark_mode_backend', -1);
+
+		$this->container->renderer->setOptions([
+			'load_fef'      => $useFEF,
+			'fef_reset'     => $fefReset,
+			'fef_dark'      => $useFEF ? $darkMode : 0,
+			// Render submenus as drop-down navigation bars powered by Bootstrap
+			'linkbar_style' => 'classic',
+		]);
+
+		return true;
+	}
+
 }
--- component/frontend/Dispatcher/Dispatcher.php
+++ component/frontend/Dispatcher/Dispatcher.php
@@ -22,4 +22,26 @@ class Dispatcher extends FOFDispatcher
 		parent::__construct($container, $config);
 	}
 
+	public function onBeforeDispatch(): void
+	{
+		// Renderer options (0=none, 1=frontend, 2=backend, 3=both)
+		$useFEF   = in_array($this->container->params->get('load_fef', 3), [1, 3]);
+		$fefReset = $useFEF && in_array($this->container->params->get('fef_reset', 3), [1, 3]);
+
+		if (!$useFEF)
+		{
+			$this->container->rendererClass = '\\FOF30\\Render\\Joomla3';
+		}
+
+		$darkMode = $this->container->params->get('dark_mode_frontend', -1);
+
+		$this->container->renderer->setOptions([
+			'load_fef'      => $useFEF,
+			'fef_reset'     => $fefReset,
+			'fef_dark'      => $useFEF ? $darkMode : 0,
+			// Render submenus as drop-down navigation bars powered by Bootstrap
+			'linkbar_style' => 'classic',
+		]);
+	}
+
 }
