44fc76a7902872354a7dc8313d72e0345a0d5c92
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-06-09 17:12:14 +0300

    Make things configurable
    Implement import and export since subform does not support
    default values.

--- component/backend/Model/ControlPanel.php
+++ component/backend/Model/ControlPanel.php
@@ -8,10 +8,68 @@
 namespace Akeeba\Compatibility\Admin\Model;
 
 use FOF30\Model\Model;
+use Joomla\CMS\Language\Text;
+use RuntimeException;
 
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
 class ControlPanel extends Model
 {
+	public function importFromFileDescriptor(?array $file): void
+	{
+		// Sanity checks
+		if (!$file)
+		{
+			throw new RuntimeException(Text::_('COM_COMPATIBILITY_CONTROLPANEL_ERR_NOFILE'));
+		}
+
+		$data = file_get_contents($file['tmp_name']);
+
+		if ($data === false)
+		{
+			throw new RuntimeException(Text::_('COM_COMPATIBILITY_CONTROLPANEL_ERR_EMPTYFILE'));
+		}
+
+		$data = json_decode($data, false);
+
+		if (is_null($data))
+		{
+			throw new RuntimeException(Text::_('COM_COMPATIBILITY_CONTROLPANEL_ERR_NOTJSON'));
+		}
+
+		$validProperties = [
+			'extensions',
+			'cms',
+			'exclude_php',
+			'show_intro',
+			'intro_text',
+			'load_fef',
+			'fef_reset',
+			'dark_mode_backend',
+			'dark_mode_frontend',
+		];
+		$importedCount   = 0;
+
+		$params = $this->container->params;
+
+		foreach ($validProperties as $prop)
+		{
+			if (!isset($data->{$prop}))
+			{
+				continue;
+			}
+
+			$importedCount++;
+
+			$params->set($prop, $data->{$prop});
+		}
+
+		if (!$importedCount)
+		{
+			throw new RuntimeException('COM_COMPATIBILITY_CONTROLPANEL_ERR_NODATA');
+		}
+
+		$params->save();
+	}
 }
--- component/backend/View/ControlPanel/tmpl/default.blade.php
+++ component/backend/View/ControlPanel/tmpl/default.blade.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package		com_compatibility
- * @copyright	Copyright (c)2017-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license		GNU General Public License version 3 or later
+ * @package        com_compatibility
+ * @copyright      Copyright (c)2017-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license        GNU General Public License version 3 or later
  */
 
 /** @var \FOF30\View\DataView\Html $this */
@@ -13,4 +13,40 @@ defined('_JEXEC') or die();
 
 <div class="akeeba-block--info">
     @lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_HOWTOUSE')
+</div>
+
+<div class="akeeba-panel--default">
+    <header class="akeeba-block-header">
+        <h3>@lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_IMPORTEXPORT')</h3>
+    </header>
+
+    <p>@lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_WHYIMPORTEXPORT')</p>
+
+    <form name="adminForm" id="adminForm" class="akeeba-form"
+          action="index.php" method="post" enctype="multipart/form-data">
+
+        <div class="akeeba-form-group">
+            <label>@lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_IMPORTFILE')</label>
+
+            <input type="file" name="importfile" value="" />
+        </div>
+
+        <div class="akeeba-form-group--actions">
+            <div class="akeeba-container--50-50">
+                <button type="submit" class="akeeba-btn--orange--block">
+                    <span class="akion-android-upload"></span>
+                    @lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_IMPORT')
+                </button>
+                <a class="akeeba-btn--success--block" href="@route('index.php?option=com_compatibility&view=ControlPanel&task=export&format=json')">
+                    <span class="akion-code-download"></span>
+                    @lang('COM_COMPATIBILITY_CONTROLPANEL_LBL_EXPORT')
+                </a>
+            </div>
+        </div>
+
+        <input type="hidden" name="option" value="{{ $this->getContainer()->componentName }}" />
+        <input type="hidden" name="view" value="{{ $this->input->get('view') }}" />
+        <input type="hidden" name="task" value="import" />
+        <input type="hidden" name="<?php echo $this->container->platform->getToken(true); ?>" value="1" />
+    </form>
 </div>
